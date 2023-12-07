94fabd6be4b5881b00bbf363d60e3054a9ff1c89
gurpreetsinghwazir <143097451+gurpreetsinghwazir@users.noreply.github.com>
2023-12-07 15:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+<script>
+// Get the modal
+var modal = document.getElementById('one');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
